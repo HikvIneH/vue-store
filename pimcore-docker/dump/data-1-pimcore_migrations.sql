ee0a5cbf7950cff7d1231a2ffb55d1d3c7edc758
Hikvineh <rarifky.aditya@gmail.com>
2019-03-21 20:27:43 +0700

    data dump

---
+++ pimcore-docker/dump/data-1-pimcore_migrations.sql
@@ -0,0 +1,15 @@
+
+SET NAMES utf8mb4;
+
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('pimcore_core','20180724144005','2018-07-26 14:00:13');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('pimcore_core','20180830113528','2018-09-07 12:25:43');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('pimcore_core','20180830122128','2018-09-07 12:25:43');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('pimcore_core','20180904201947','2018-09-07 15:06:46');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('PimcoreCustomerManagementFrameworkBundle','00000001','2017-11-20 17:32:44');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('PimcoreCustomerManagementFrameworkBundle','20171019130865','2017-11-10 13:52:45');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('PimcoreCustomerManagementFrameworkBundle','20171102160547','2017-11-10 13:52:45');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('PimcoreCustomerManagementFrameworkBundle','20171113110855','2017-11-20 17:32:44');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('PimcoreCustomerManagementFrameworkBundle','20171207150300','2017-12-19 09:41:33');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('PimcoreCustomerManagementFrameworkBundle','20171218194028','2017-12-21 14:30:49');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('PimcoreCustomerManagementFrameworkBundle','20180102103153','2018-01-02 14:30:49');
+INSERT INTO pimcore_migrations (`migration_set`,`version`,`migrated_at`) VALUES ('PimcoreEcommerceFrameworkBundle','00000001','2018-02-13 12:10:28');
